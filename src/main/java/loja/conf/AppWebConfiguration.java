8ebe8cdf1375678928cb14c05162673cf5c4d169
Snow <pedroh.1000@gmail.com>
2020-12-29 11:01:35 -0300

    Capítulo 16

--- src/main/java/loja/conf/AppWebConfiguration.java
+++ src/main/java/loja/conf/AppWebConfiguration.java
@@ -21,6 +21,7 @@ import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -113,4 +114,8 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 		return new CookieLocaleResolver();
 	}
 
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 }
